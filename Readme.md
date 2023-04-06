2a41c6e842e615687725d8ba2cea5b3e9b3a0e1e
Stieilijh <saitushar02@gmail.com>
2023-04-06 20:42:10 +0530

    Added music player slider (completely working )

--- Readme.md
+++ Readme.md
@@ -4,3 +4,5 @@ I wrote this python program using tkinter and pygame.
 Right now we can Select a song, Play a song, Restart it, Pause(using the button and the space bar), Control the Volume of the song.
 
 **Many more updates in the future !!!**
+
+# Note : Only mp3 files can be played
--- UI.py
+++ UI.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import music_player
 import pygame
 from tkinter import filedialog
 import os
@@ -9,26 +8,28 @@ pygame.mixer.init()
 
 WINDOW_WIDTH = "600"
 WINDOW_LENGTH = "400"
-TEMPFILE = ""
+SONG_LENGTH = 0
+MUSIC_SLIDER_CLICKED = False
+SONG_POSITION = 0
 
 
 def on_select_btn_clicked():
     file_path = filedialog.askopenfilename(title="Open a music file",
-                                           filetypes=(("mp3 files", "*.mp3"),
-                                                      ("wav files", "*.wav")))
+                                           filetypes=(("mp3 files", "*.mp3")))
     if not os.path.exists(str(file_path)):
         return
-    global TEMPFILE
-    TEMPFILE = file_path
     select_song(file_path)
 
 
 def select_song(file_path):
-    output_file = music_player.convert(file_path)
-    pygame.mixer.music.load(output_file)
+    global SONG_LENGTH
+    pygame.mixer.music.load(file_path)
+    SONG_LENGTH = pygame.mixer.Sound(file_path).get_length()
 
 
 def on_play_btn_clicked():
+    global SONG_POSITION
+    SONG_POSITION = 0
     pygame.mixer.music.play()
 
 
@@ -43,10 +44,35 @@ def on_volume_slider_change(value):
     pygame.mixer.music.set_volume(int(value)/100)
 
 
+def update_music_slider():
+    global SONG_LENGTH
+    if pygame.mixer.music.get_busy() and SONG_LENGTH != 0 and not MUSIC_SLIDER_CLICKED:
+        time = SONG_POSITION/SONG_LENGTH/10
+        mus_slider.set(time)
+    window.after(900, update_music_slider)
+
+
+def on_music_slider_change(pvalue):
+    global SONG_LENGTH, MUSIC_SLIDER_CLICKED, SONG_POSITION
+    if pygame.mixer.music.get_busy() and SONG_LENGTH != 0:
+        value = mus_slider.get()
+        position = int(int(value)*10*SONG_LENGTH)/1000
+        pygame.mixer.music.play()
+        pygame.mixer.music.set_pos(position)
+        SONG_POSITION = position * 1000
+        MUSIC_SLIDER_CLICKED = False
+
+
+def update_pos_variable():
+    global SONG_POSITION
+    if pygame.mixer.music.get_busy() and SONG_LENGTH != 0:
+        SONG_POSITION = SONG_POSITION+200
+    window.after(200, update_pos_variable)
+
+
 def on_closing():
-    global TEMPFILE
-    if (os.path.exists(str(TEMPFILE))):
-        os.remove(str(TEMPFILE))
+    if pygame.mixer.music.get_busy:
+        pygame.mixer.music.unload()
     window.destroy()
 
 
@@ -78,12 +104,30 @@ window.bind("<space>", lambda event: on_pause_btn_clicked())
 label = tk.Label(window, text="Volume")
 label.grid(row=0, column=3)
 
-# create slider
-SLIDER_WIDTH = 30*int(WINDOW_WIDTH)/100
-slider = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL,
-                  command=on_volume_slider_change, length=SLIDER_WIDTH)
-slider.set(50)
+# create volume slider
+VOLUME_SLIDER_WIDTH = 30*int(WINDOW_WIDTH)/100
+vol_slider = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL,
+                      command=on_volume_slider_change, length=VOLUME_SLIDER_WIDTH)
+vol_slider.set(50)
 pygame.mixer.music.set_volume(0.5)
-slider.grid(row=1, column=3, columnspan=4)
+vol_slider.grid(row=1, column=3, columnspan=4)
+
+
+def on_mus_slider_click(event):
+    global MUSIC_SLIDER_CLICKED
+    MUSIC_SLIDER_CLICKED = True
+
+
+# create music slider
+MUSIC_SLIDER_WIDTH = 90*int(WINDOW_LENGTH)/100
+mus_slider = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL,
+                      length=MUSIC_SLIDER_WIDTH)
+mus_slider.set(0)
+mus_slider.bind("<Button-1>", on_mus_slider_click)
+mus_slider.bind("<ButtonRelease-1>", on_music_slider_change)
+mus_slider.grid(row=4, column=1, columnspan=4)
+# processes
+window.after(1000, update_music_slider)
+window.after(200, update_pos_variable)
 # Start the event loop
 window.mainloop()
