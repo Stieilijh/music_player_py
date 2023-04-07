844b5f4331bb52b359e97ffd22df9f5878ed8028
Stieilijh <saitushar02@gmail.com>
2023-04-07 10:24:51 +0530

    show minute and second instead of percentage

--- UI.py
+++ UI.py
@@ -30,13 +30,16 @@ def select_song(file_path):
     global SONG_LENGTH
     pygame.mixer.music.load(file_path)
     SONG_LENGTH = pygame.mixer.Sound(file_path).get_length()
+    mus_slider.configure(to=SONG_LENGTH)
+    on_restart_btn_clicked()
 
 
-def on_play_btn_clicked():
+def on_restart_btn_clicked():
     global SONG_POSITION
     SONG_POSITION = 0
     pygame.mixer.music.play()
     mus_slider.configure(state="normal")
+    change_mus_slider_label()
 
 
 def on_pause_btn_clicked():
@@ -51,31 +54,51 @@ def on_pause_btn_clicked():
 def on_volume_slider_change(value):
     pygame.mixer.music.set_volume(int(value)/100)
 
-
-def update_music_slider():
-    global SONG_LENGTH
-    if pygame.mixer.music.get_busy() and SONG_LENGTH != 0 and not MUSIC_SLIDER_CLICKED:
-        time = SONG_POSITION/SONG_LENGTH/10
-        mus_slider.set(time)
-    window.after(900, update_music_slider)
-
-
 def on_music_slider_change(pvalue):
     global SONG_LENGTH, MUSIC_SLIDER_CLICKED, SONG_POSITION
     if pygame.mixer.music.get_busy() and SONG_LENGTH != 0:
         value = mus_slider.get()
-        position = int(int(value)*10*SONG_LENGTH)/1000
+        position = value
         pygame.mixer.music.play()
         pygame.mixer.music.set_pos(position)
         SONG_POSITION = position * 1000
+        change_mus_slider_label()
     MUSIC_SLIDER_CLICKED = False
 
+def on_mus_slider_click(event):
+    global MUSIC_SLIDER_CLICKED
+    MUSIC_SLIDER_CLICKED = True
+
+def change_mus_slider_label():
+    if SONG_LENGTH==0:
+        return
+    value=convert_seconds_to_minutes(SONG_POSITION//1000)+"/"+convert_seconds_to_minutes(SONG_LENGTH)
+    mus_slider_label.configure(text=value)
+
+def convert_seconds_to_minutes(seconds):
+    seconds=int(seconds)
+    m=str(seconds//60)
+    s=seconds%60
+    if s<10:
+        s="0"+str(s)
+    else:
+        s=str(s)
+    return m+":"+s
+
+#Process Methods
+def update_music_slider():
+    global SONG_LENGTH
+    if pygame.mixer.music.get_busy() and SONG_LENGTH != 0 and not MUSIC_SLIDER_CLICKED:
+        time = SONG_POSITION/1000
+        mus_slider.set(time)
+        change_mus_slider_label()
+    window.after(1000, update_music_slider)
 
 def update_pos_variable():
     global SONG_POSITION
     if pygame.mixer.music.get_busy() and SONG_LENGTH != 0:
-        SONG_POSITION = SONG_POSITION+200
-    window.after(200, update_pos_variable)
+        SONG_POSITION = SONG_POSITION+100
+    window.after(100, update_pos_variable)
 
 
 def on_closing():
@@ -96,12 +119,12 @@ window.geometry(WINDOW_WIDTH+"x"+WINDOW_LENGTH)
 window.title("Music Player")
 
 # Create button
-select_button = tk.Button(window, text="Select",
+select_button = tk.Button(window, text="Select & Play",
                           command=on_select_btn_clicked)
 select_button.grid(row=0, column=0)
-play_button = tk.Button(window, text="Play/Restart",
-                        command=on_play_btn_clicked)
-play_button.grid(row=0, column=1)
+restart_button = tk.Button(window, text="Restart",
+                        command=on_restart_btn_clicked)
+restart_button.grid(row=0, column=1)
 pause_button = tk.Button(window, text="Pause", command=on_pause_btn_clicked)
 pause_button.grid(row=0, column=2)
 
@@ -120,22 +143,18 @@ vol_slider.set(50)
 pygame.mixer.music.set_volume(0.5)
 vol_slider.grid(row=1, column=3, columnspan=4)
 
-
-def on_mus_slider_click(event):
-    global MUSIC_SLIDER_CLICKED
-    MUSIC_SLIDER_CLICKED = True
-
-
-# create music slider
+# create music slider and label
+mus_slider_label = tk.Label(window,text="0:00/0:00")
+mus_slider_label.grid(row=4,column=1)
 MUSIC_SLIDER_WIDTH = 90*int(WINDOW_LENGTH)/100
 mus_slider = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL,
-                      length=MUSIC_SLIDER_WIDTH)
+                      length=MUSIC_SLIDER_WIDTH,tickinterval=0,showvalue=0)
 mus_slider.set(0)
 mus_slider.bind("<Button-1>", on_mus_slider_click)
 mus_slider.bind("<ButtonRelease-1>", on_music_slider_change)
-mus_slider.grid(row=4, column=1, columnspan=4)
+mus_slider.grid(row=6, column=1, columnspan=4)
 # processes
 window.after(1000, update_music_slider)
-window.after(200, update_pos_variable)
+window.after(100, update_pos_variable)
 # Start the event loop
 window.mainloop()
